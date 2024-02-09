bbdcfe272fc67f567dce5e56759686947b979491
Dayz1993 <156677045+Dayz1993@users.noreply.github.com>
2024-02-09 19:00:59 +0000

    app.py

--- frontend/app.py
+++ frontend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -14,8 +14,26 @@ def about():
 def team():
     return render_template('team.html')
 
+@app.route('/search', methods=['POST'])
+def search_result():
+    if request.method == "POST":
+        query = request.form['query']
+    
+        return render_template('search_result.html', query = query)
+    else:
+        return "Method not Allowed"
 
-
+@app.route('/search', methods=['POST'])
+def process_button():
+    if 'timeline' in request.form:
+        query = request.form['query']
+        return render_template('search_result.html', query = query)
+    
+    elif 'news' in request.form:
+        return render_template('team.html')
+    
+   
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    #app.run(debug=True)
+    app.run(host='192.168.0.48', port=5000, debug=True, threaded=False)
