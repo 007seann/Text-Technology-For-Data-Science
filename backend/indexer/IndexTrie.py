974931bcc641db8f50f00212cf747dc432d7f5cc
Martin Namukombo <mastadict@gmail.com>
2024-02-12 14:22:49 +0000

    Update IndexTrie.py

--- backend/indexer/IndexTrie.py
+++ backend/indexer/IndexTrie.py
@@ -158,10 +158,3 @@ if __name__ == "__main__":
     for word in wordindex.traverse_words():
         print(word)
     wordindex.visualize_word_index()
-
-
-
-index trie
-
-
-insert(word, doc_id, ll)
